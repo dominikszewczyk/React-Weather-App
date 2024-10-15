1d8daf5747f1a2f03678ab99557dd6bf8f4fd93a
Dominik Szewczyk <dominikszewczyklim@gmail.com>
2024-10-15 14:40:48 +0200

    Adding useThemeDetecor hook

--- src/components/Weather/Weather.jsx
+++ src/components/Weather/Weather.jsx
@@ -1,14 +1,20 @@
+import { useEffect } from 'react'
+
 import Navbar from '../Navbar/Navbar'
 import Current from '../Current/Current'
 import Details from '../Details/Details'
 import Summary from '../Summary/Summary'
 import Forecast from '../Forecast/Forecast'
+
+import useThemeDetector from '../../hooks/useThemeDetector'
+
 import './Weather.style.scss'
 
 export default function Weather() {
+    const theme = useThemeDetector();
     return (
         <>
-            <div className="weather">
+            <div className={`weather ${theme}`}>
             <Navbar />
 
                 <div className="weather__main">
